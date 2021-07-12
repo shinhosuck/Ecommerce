51973431fba8662a075bd336be8ef85a8613d10e
shinhosuck <shinhosuck1973@gmail.com>
2021-07-12 18:55:44 +0800

    update

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/anderson/Desktop/E-Commerce/venv/bin/python3
+#!/home/anderson/Documents/Ecommerce/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
